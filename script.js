541e0b836d2484b781f3fa6b977b913a972e6ecb
kfir <kfirrp@gmail.com>
2021-03-14 22:06:25 +0200

    back up - contact.html included

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 setInterval(function () { 
-    document.getElementById("interval").style.display = "none";
-    document.getElementById('second-interval').style.display = "flex";  
-    document.getElementById('second-interval').style.flexDirection = "column";  
+    if(document.getElementById("interval").style.display === 'none') {
+        document.getElementById("interval").style.display = "flex";
+        document.getElementById('second-interval').style.display = "none";  
+    }
+    else{
+        document.getElementById("interval").style.display = "none";
+        document.getElementById('second-interval').style.display = "flex";  
+        document.getElementById('second-interval').style.flexDirection = "column";  
+    }
+    
+
 },2000);
