176071f15a83f92eb7088f2943f47618c4ff3049
kfir <kfirrp@gmail.com>
2021-03-14 09:36:23 +0200

    js update (onload did not work), dropDown menu on top of pics fix, fixed footer works

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// function setInt(){
-//     var slideOne = document.getElementsById('interval')
-//     setInterval(function setInt(){ element.classList.add("second-interval"); }, 2000);
-// }
+setInterval(function () { 
+    document.getElementById("interval").style.display = "none";
+    document.getElementById('second-interval').style.display = "flex";  
+    document.getElementById('second-interval').style.flexDirection = "column";  
+},2000);
