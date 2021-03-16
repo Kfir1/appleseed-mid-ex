6a7a1f3fbb721dd41467b3f78bda608bc874f79d
kfir <kfirrp@gmail.com>
2021-03-16 11:33:51 +0200

    renamed index.html

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-// setInterval(function () { 
-//     if(document.getElementById("interval").style.display === 'none') {
-//         document.getElementById("interval").style.display = "block";
-//         document.getElementById('second-interval').style.display = "none";  
-//     }
-//     else{
-//         document.getElementById("interval").style.display = "none";
-//         document.getElementById('second-interval').style.display = "flex";  
-//         document.getElementById('second-interval').style.flexDirection = "column";  
-//     }
-  
-
-// },1000);
-
-
-
 setInterval(function () { 
-   
-
-    setTimeout(function(){ 
-        
-       
+    if(document.getElementById("interval").style.display === 'none') {
+        document.getElementById("interval").style.display = "block";
+        document.getElementById('second-interval').style.display = "none";  
+    }
+    else{
         document.getElementById("interval").style.display = "none";
-        document.getElementById("third-interval").style.display = "none";
-        document.getElementById('second-interval').style.display = "block";  
         document.getElementById('second-interval').style.display = "flex";  
         document.getElementById('second-interval').style.flexDirection = "column";  
+    }
+  
+
+},2000);
+
+
+
+// setInterval(function () { 
+   
+
+//     setTimeout(function(){ 
+        
+       
+//         document.getElementById("interval").style.display = "none";
+//         document.getElementById("third-interval").style.display = "none";
+//         document.getElementById('second-interval').style.display = "block";  
+//         document.getElementById('second-interval').style.display = "flex";  
+//         document.getElementById('second-interval').style.flexDirection = "column";  
    
     
 
     
-        }, 2000);
-    setTimeout(function(){ 
+//         }, 2000);
+//     setTimeout(function(){ 
         
        
-                document.getElementById("interval").style.display = "none";
-                document.getElementById("third-interval").style.display = "none";
-                document.getElementById('second-interval').style.display = "block";  
-                document.getElementById('second-interval').style.display = "flex";  
-                document.getElementById('second-interval').style.flexDirection = "column";  
+//                 document.getElementById("interval").style.display = "none";
+//                 document.getElementById("third-interval").style.display = "none";
+//                 document.getElementById('second-interval').style.display = "block";  
+//                 document.getElementById('second-interval').style.display = "flex";  
+//                 document.getElementById('second-interval').style.flexDirection = "column";  
            
             
    
             
-         }, 2000);
+//          }, 2000);
         
-         setTimeout(function(){ 
-            document.getElementById("interval").style.display = "none";
-            document.getElementById("second-interval").style.display = "none";
-            document.getElementById('third-interval').style.display = "flex";  
-            document.getElementById('third-interval').style.flexDirection = "column"; 
-            document.getElementById('third-interval').style.display = "block";
-        }, 2000);
+//          setTimeout(function(){ 
+//             document.getElementById("interval").style.display = "none";
+//             document.getElementById("second-interval").style.display = "none";
+//             document.getElementById('third-interval').style.display = "flex";  
+//             document.getElementById('third-interval').style.flexDirection = "column"; 
+//             document.getElementById('third-interval').style.display = "block";
+//         }, 2000);
 
-        document.getElementById("interval").style.display = "block";
-        document.getElementById("third-interval").style.display = "none";
-        document.getElementById("second-interval").style.display = "none";
-},2000);
+//         document.getElementById("interval").style.display = "block";
+//         document.getElementById("third-interval").style.display = "none";
+//         document.getElementById("second-interval").style.display = "none";
+// },2000);
 
 
 
